b4d585611e48ed97978019f296c8bbaa2dbd36ef
hectorstudio <david.liu987@hotmail.com>
2019-04-10 03:44:15 -0500

    Added Page component

---
+++ src/Page/Page.tsx
@@ -0,0 +1,106 @@
+import * as React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+import { IPageProps, IPageState, TypeComponent, TypeStyle } from './../../index'
+import styles from './styles'
+
+export default class Page extends React.Component<IPageProps, IPageState> {
+  public render(): TypeComponent {
+    return(
+      <View style={ this._processStyle() }>
+        { this.Image() }
+        { this.Title() }
+        { this.Subtitle() }
+      </View>
+    )
+  }
+
+  public Image() {
+    const { image, imageContainerStyle } = this._processProps()
+    const props = {
+      style: StyleSheet.flatten([styles.imageContainer, imageContainerStyle])
+    }
+
+    return (
+      <View { ...props }>{ image }</View>
+    )
+  }
+
+  public Title(): TypeComponent {
+    const { allowFontScaling, isLight, title, titleStyle } = this._processProps()
+
+    if (this._isComponent(title)) {
+      return React.cloneElement(title as any, { style: titleStyle })
+    }
+
+    const props = {
+      allowFontScaling,
+      style: StyleSheet.flatten([styles.title, isLight && styles.titleLight, titleStyle]),
+    }
+
+    return (
+      <View style={ [styles.padding] }>
+        <Text { ...props }>{ title }</Text>
+      </View>
+    )
+  }
+
+  public Subtitle(): TypeComponent {
+    const { allowFontScaling, isLight, subtitle, subtitleStyle } = this._processProps()
+
+    if (this._isComponent(subtitle)) {
+      return React.cloneElement(subtitle as any, { style: subtitleStyle })
+    }
+
+    const props = {
+      allowFontScaling,
+      style: StyleSheet.flatten([styles.subtitle, isLight && styles.subtitleLight, subtitleStyle]),
+    }
+
+    return (
+      <View style={styles.padding}>
+        <Text { ...props }>{ subtitle }</Text>
+      </View>
+    )
+  }
+
+  private _processProps(): IPageState {
+    // tslint:disable-next-line: no-shadowed-variable
+    const { allowFontScaling, containerStyle, height, image, imageContainerStyle, isLight, options, style, subtitle, subtitleStyle, title, titleStyle, width } = this.props
+
+    const props: IPageState = {
+      allowFontScaling: (options && options.allowFontScaling) || (allowFontScaling || true),
+      containerStyle: (options && options.containerStyle) || (containerStyle || undefined),
+      height: (options && options.height) || (height || undefined),
+      image: (options && options.image) || (image || undefined),
+      imageContainerStyle: (options && options.imageContainerStyle) || (imageContainerStyle || undefined),
+      isLight: (options && options.isLight) || (isLight || false),
+      style: (options && options.style) || (style || undefined),
+      subtitle: (options && options.subtitle) || (subtitle || undefined),
+      subtitleStyle: (options && options.subtitleStyle) || (subtitleStyle || undefined),
+      title: (options && options.title) || (title || undefined),
+      titleStyle: (options && options.titleStyle) || (titleStyle || undefined),
+      width: (options && options.width) || (width || undefined),
+    }
+
+    return props
+  }
+
+  private _processStyle(): TypeStyle {
+    const { containerStyle, height, width } = this._processProps()
+    const _style: TypeStyle = {}
+
+    return StyleSheet.flatten([styles.container, _style, containerStyle, { height, width }])
+  }
+
+  /**
+   * Method to valid if a object is a component
+   * @private
+   * @param {*} kind Object to validate
+   * @returns {boolean}
+   * @memberof Page
+   */
+  private _isComponent(kind: any): boolean {
+    return (kind._owner && kind._owner.constructor.name === 'FiberNode' && kind.$$typeof && kind.$$typeof.constructor.name === 'Symbol')
+  }
+}
---
+++ src/Page/index.d.ts
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import { ViewProps } from 'react-native'
+
+import { TypeComponent, TypeStyle } from './../../index'
+
+/**
+ * Interface to define the states of the Page component
+ * @export
+ * @interface IPageState
+ */
+export interface IPageState extends ViewProps {
+  allowFontScaling?: boolean
+  containerStyle?: TypeStyle
+  height?: number
+  image: TypeComponent
+  imageContainerStyle?: TypeStyle
+  isLight?: boolean
+  title: string | TypeComponent
+  titleStyle?: TypeStyle
+  subtitle?: string | TypeComponent
+  subtitleStyle?: TypeStyle
+  width?: number
+}
+
+/**
+ * Interface to define the props of the Page component
+ * @export
+ * @interface IPageProps
+ * @extends {IPageState}
+ */
+export interface IPageProps extends IPageState {
+  /**
+   * Prop for group all the props of the Page component
+   */
+  options?: IPageState
+}
+
+/**
+ * Declaration for Page component
+ * @class Page
+ * @extends {React.Component<IPageProps, IPageState>}
+ */
+declare class Page extends React.Component<IPageProps, IPageState> {
+}
+
+/**
+ * Export default
+ */
+export default Page
