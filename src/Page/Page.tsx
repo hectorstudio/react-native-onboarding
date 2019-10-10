936150d09d8c91e9dd85798a0aa1e7f228e538ac
hectorstudio <david.liu987@hotmail.com>
2019-10-09 21:18:58 -0500

    Improve page component

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Dimensions } from 'react-native'
 
-import { AppHelper } from '@ticmakers-react-native/core'
-import { IPageProps, IPageState, TypeComponent, TypeStyle } from './../../index'
+import Image, { TypeImageSource, IImageProps } from '@ticmakers-react-native/image'
+import { AppHelper, TypeComponent, TypeStyle } from '@ticmakers-react-native/core'
+
+import { IPageProps, IPageState } from './../../index'
 import styles from './styles'
 
 /**
@@ -11,58 +13,133 @@ import styles from './styles'
  * @extends {React.Component<IPageProps, IPageState>}
  */
 export default class Page extends React.Component<IPageProps, IPageState> {
+  constructor(props: IPageProps) {
+    super(props)
+
+    const { height, width } = this._processProps()
+    this.state = {
+      height,
+      width,
+    }
+  }
+
   /**
    * Method that renders the component
    * @returns {TypeComponent}
-   * @memberof Page
    */
   public render(): TypeComponent {
-    return(
+    return (
       <View style={ this._processStyle() }>
-        { this.Header() }
-        { this.Image() }
-        { this.Title() }
-        { this.Subtitle() }
+        { this.ImageBackground() }
+
+        <View style={ styles.contentPage }>
+          { this.Header() }
+
+          <View style={ styles.bodyPage }>
+            { this.Image() }
+            { this.Title() }
+            { this.Subtitle() }
+          </View>
+        </View>
       </View>
     )
+  }
+
+  public ImageBackground(): TypeComponent {
+    const { backgroundImage } = this._processProps()
+    const { height, width } = this.state
+
+    if (typeof backgroundImage !== 'undefined') {
+      return (
+        <Image
+          source={ backgroundImage }
+          style={ StyleSheet.flatten([styles.backgroundImage, { height, width }]) }
+        />
+      )
+    }
   }
 
   /**
    * Method that renders the Header component
    * @returns {TypeComponent}
-   * @memberof Page
    */
   public Header(): TypeComponent {
     const { header, headerContainerStyle } = this._processProps()
+
     const props = {
       style: StyleSheet.flatten([styles.headerContainer, headerContainerStyle]),
     }
 
-    return (
-      <View { ...props }>{ header }</View>
-    )
+    const headerProps = {
+      style: StyleSheet.flatten([{ resizeMode: 'contain', height: 160 }]),
+    }
+
+    if (AppHelper.isComponent(header)) {
+      return (
+        <View { ...props }>
+          { React.cloneElement(header as any) }
+        </View>
+      )
+    }
+
+    if (typeof header === 'number' || header && (header as any).uri) {
+      return (
+        <View { ...props }>
+          { <Image source={ header as TypeImageSource } { ...headerProps } /> }
+        </View>
+      )
+    }
+
+    if (typeof header !== 'undefined') {
+      return (
+        <View { ...props }>{ header }</View>
+      )
+    }
   }
 
   /**
    * Method that renders the Image component
    * @returns {TypeComponent}
-   * @memberof Page
    */
   public Image(): TypeComponent {
     const { image, imageContainerStyle } = this._processProps()
+
     const props = {
       style: StyleSheet.flatten([styles.imageContainer, imageContainerStyle]),
     }
 
-    return (
-      <View { ...props }>{ image }</View>
-    )
+    const imageProps = {
+      style: StyleSheet.flatten([{ resizeMode: 'contain', height: 160 }]),
+    }
+
+    if (AppHelper.isComponent(image)) {
+      return (
+        <View { ...props }>
+          { React.cloneElement(image as any) }
+        </View>
+      )
+    }
+
+    if (typeof image === 'number' || image && (image as any).uri) {
+      return (
+        <View { ...props }>
+          { <Image source={ image as TypeImageSource } { ...imageProps } /> }
+        </View>
+      )
+    }
+
+    if (typeof image !== 'undefined') {
+      return (
+        <View { ...props }>
+          { <Image { ...image as IImageProps } { ...imageProps } /> }
+        </View>
+      )
+    }
   }
 
   /**
    * Method that renders the Title component
    * @returns {TypeComponent}
-   * @memberof Page
    */
   public Title(): TypeComponent {
     const { allowFontScaling, isLight, title, titleStyle } = this._processProps()
@@ -76,17 +153,18 @@ export default class Page extends React.Component<IPageProps, IPageState> {
       style: StyleSheet.flatten([styles.title, isLight && styles.titleLight, titleStyle]),
     }
 
-    return (
-      <View style={ [styles.padding] }>
-        <Text { ...props }>{ title }</Text>
-      </View>
-    )
+    if (title) {
+      return (
+        <View style={ [styles.padding] }>
+          <Text { ...props }>{ title }</Text>
+        </View>
+      )
+    }
   }
 
   /**
    * Method that renders the Subtitle component
    * @returns {TypeComponent}
-   * @memberof Page
    */
   public Subtitle(): TypeComponent {
     const { allowFontScaling, isLight, subtitle, subtitleStyle } = this._processProps()
@@ -100,37 +178,39 @@ export default class Page extends React.Component<IPageProps, IPageState> {
       style: StyleSheet.flatten([styles.subtitle, isLight && styles.subtitleLight, subtitleStyle]),
     }
 
-    return (
-      <View style={ styles.padding }>
-        <Text { ...props }>{ subtitle }</Text>
-      </View>
-    )
+    if (subtitle) {
+      return (
+        <View style={ styles.padding }>
+          <Text { ...props }>{ subtitle }</Text>
+        </View>
+      )
+    }
   }
 
   /**
    * Method that process the props of the component
    * @private
    * @returns {IPageState}
-   * @memberof Page
    */
-  private _processProps(): IPageState {
-    const { allowFontScaling, containerStyle, header, headerContainerStyle, height, image, imageContainerStyle, isLight, options, style, subtitle, subtitleStyle, title, titleStyle, width } = this.props
+  private _processProps(): IPageProps {
+    const { allowFontScaling, backgroundImage, containerStyle, header, headerContainerStyle, height, image, imageContainerStyle, isLight, style, subtitle, subtitleStyle, title, titleStyle, width } = this.props
 
-    const props: IPageState = {
-      allowFontScaling: (options && options.allowFontScaling) || (allowFontScaling || true),
-      containerStyle: (options && options.containerStyle) || (containerStyle || undefined),
-      header: (options && options.header) || (header || undefined),
-      headerContainerStyle: (options && options.headerContainerStyle) || (headerContainerStyle || undefined),
-      height: (options && options.height) || (height || undefined),
-      image: (options && options.image) || (image || undefined),
-      imageContainerStyle: (options && options.imageContainerStyle) || (imageContainerStyle || undefined),
-      isLight: (options && options.isLight) || (isLight || false),
-      style: (options && options.style) || (style || undefined),
-      subtitle: (options && options.subtitle) || (subtitle || undefined),
-      subtitleStyle: (options && options.subtitleStyle) || (subtitleStyle || undefined),
-      title: (options && options.title) || (title || undefined),
-      titleStyle: (options && options.titleStyle) || (titleStyle || undefined),
-      width: (options && options.width) || (width || undefined),
+    const props: IPageProps = {
+      allowFontScaling: (typeof allowFontScaling !== 'undefined' ? allowFontScaling : true),
+      backgroundImage: (typeof backgroundImage !== 'undefined' ? backgroundImage : undefined),
+      containerStyle: (typeof containerStyle !== 'undefined' ? containerStyle : undefined),
+      header: (typeof header !== 'undefined' ? header : undefined),
+      headerContainerStyle: (typeof headerContainerStyle !== 'undefined' ? headerContainerStyle : undefined),
+      height: (typeof height !== 'undefined' ? height : Dimensions.get('screen').height),
+      image: (typeof image !== 'undefined' ? image : undefined),
+      imageContainerStyle: (typeof imageContainerStyle !== 'undefined' ? imageContainerStyle : undefined),
+      isLight: (typeof isLight !== 'undefined' ? isLight : false),
+      style: (typeof style !== 'undefined' ? style : undefined),
+      subtitle: (typeof subtitle !== 'undefined' ? subtitle : undefined),
+      subtitleStyle: (typeof subtitleStyle !== 'undefined' ? subtitleStyle : undefined),
+      title: (typeof title !== 'undefined' ? title : undefined),
+      titleStyle: (typeof titleStyle !== 'undefined' ? titleStyle : undefined),
+      width: (typeof width !== 'undefined' ? width : Dimensions.get('screen').width),
     }
 
     return props
@@ -140,10 +220,10 @@ export default class Page extends React.Component<IPageProps, IPageState> {
    * Method to process the prop style of the component
    * @private
    * @returns {TypeStyle}
-   * @memberof Page
    */
   private _processStyle(): TypeStyle {
-    const { containerStyle, height, width } = this._processProps()
+    const { containerStyle } = this._processProps()
+    const { height, width } = this.state
     const _style: TypeStyle = {}
 
     return StyleSheet.flatten([styles.container, _style, containerStyle, { height, width }])
